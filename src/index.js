f2ecae5d80107d441cd3c4d8a78627bb7df3c388
brunotag <bruno.tagliapietra@gmail.com>
2018-09-02 18:11:24 +1200

    create breakpoint

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
+//debugger //uncomment this to create a breakpoint and see sourcemaps working.
 console.log(`I would pay ${courseValue} for this awesome course!`);
