8880224878d886bc683a7f459f3b80ddcde39bd4
brunotag <bruno.tagliapietra@gmail.com>
2018-09-02 23:23:54 +1200

    ESLint and ESLintWatch

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
 import chalk from 'chalk';
 
-console.log(chalk.green(`Starting app in dev mode... 3 squared is ${3**2}...`));
+console.log(chalk.green(`Starting app in dev mode... 3 squared is ${3**2}...`)); // eslint-disable-line no-console
