4f8203d47343aef63fea0585fd2067951ee4f12e
brunotag <bruno.tagliapietra@gmail.com>
2018-09-05 23:51:19 +1200

    use Fetch with polyfill for everyone

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
+import {getUsers} from './restAPI/userRestAPI'
+
 import './index.css';
 import numeral from 'numeral';
+
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody += `
+      <tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+      </tr>`
+
+      global.document.getElementById('users').innerHTML = usersBody;
+  });
+});
 
 const courseValue = numeral(1000).format('$0,0.00');
 //debugger //uncomment this to create a breakpoint and see sourcemaps working.
